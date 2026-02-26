83b5c2ceeac73876090d6a8be0c2b8dcc554dbaa
Elyssa Hallou <elyssa.hallou@universite-paris-saclay.fr>
2026-02-26 19:50:24 +0100

    Edited server.js to test virtual controller

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static(path.join(__dirname, '../Web')));
+
 io.on('connection', (socket) => {
     console.log('Un joueur s’est connecté ! ID:', socket.id);
 
     socket.on('disconnect', () => {
-        console.log('Joueur déconnecté');
+        console.log('Joueur déconnecté:', socket.id);
     });
 });
 
 const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Serveur de relais lancé sur http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Serveur La Traversée lancé sur le port ${PORT}`);
+    console.log(`Accessible en local sur http://localhost:${PORT}`);   
 });
