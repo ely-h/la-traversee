bd47f4d204cdfd09bf6903ccc18bc9fb5eb6f7c1
Sarah Ali <you@example.com>
2026-03-21 16:20:55 +0100

    correction classe

--- Game/LaTraversee/Assets/Scripts/QuarantineZone.cs
+++ Game/LaTraversee/Assets/Scripts/QuarantineZone.cs
@@ -2,23 +2,12 @@ using UnityEngine;
 
 public class QuarantineZone : MonoBehaviour
 {
-    public GameObject quarantinePrefab;
-    public float spawnInterval = 10f;//toutes les 10 secondes
-    public float zoneDuration = 5f;//la zone reste 5 secondes
-
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Lance la fonction "Spawn" de manière répétée
-        InvokeRepeating("SpawnZone", 2f, spawnInterval);
-    }
-
-    void SpawnZone()
-    {
-        //position aléatoire
-        Vector3 randomPos = new Vector3(Random.Range(-5f, 5f), Random.Range(-3f, 3f), 0);
-        GameObject newZone = Instantiate(quarantinePrefab, randomPos, Quaternion.identity);
-        Destroy(newZone, zoneDuration);
-        
-        Debug.Log("ZONE !");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Le joueur est dans la zone !");
+            other.GetComponent<SpriteRenderer>().color = Color.cyan;
+        }
     }
 }
