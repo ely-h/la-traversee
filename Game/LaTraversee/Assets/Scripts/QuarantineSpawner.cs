5f71e22bc7090cf2d7a0ad87b1f6113e54802084
Sarah Ali <you@example.com>
2026-04-01 10:15:00 +0200

    taille  de la zone

--- Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
+++ Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class QuarantineSpawner : MonoBehaviour
 {
     public GameObject quarantinePrefab;
-    public float tailleZone = 1f;
+    public float tailleZone = 2f;
 
     void Start()
     {
