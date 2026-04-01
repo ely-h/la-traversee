f48d83c44001db1be0c4bf7385fd76e5785aabb1
Sarah Ali <you@example.com>
2026-04-01 09:50:38 +0200

    gestion de la disparition de la zone

--- Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
+++ Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
@@ -10,12 +10,13 @@ public class QuarantineSpawner : MonoBehaviour
         Debug.Log("Le Spawner est ACTIF sur l'objet : " + gameObject.name);
 
         // 2. On lance le chrono (2s de délai, puis toutes les 10s)
-        InvokeRepeating("SpawnZone", 2f, 10f);
+        InvokeRepeating("SpawnZone", 10f,10f);
     }
 
     void SpawnZone()
     {
         Debug.Log("ZONE !");
         Instantiate(quarantinePrefab, Vector3.zero, Quaternion.identity);
+        Destroy(tempZone, 5f);
     }
 }
