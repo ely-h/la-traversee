c1cf8c96d7f5140b629f44638938ee695d8696ac
Sarah Ali <you@example.com>
2026-04-14 17:02:28 +0200

    Amelioration du temps et du spawn de la zone

--- Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
+++ Game/LaTraversee/Assets/Scripts/QuarantineSpawner.cs
@@ -1,24 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class QuarantineSpawner : MonoBehaviour
 {
     public GameObject quarantinePrefab;
     public float tailleZone = 2f;
 
+    private const float MAP_X_MIN = -17f;
+    private const float MAP_X_MAX = 17f;
+    private const float MAP_Y_MIN = -9.5f;
+    private const float MAP_Y_MAX = 5f;
+
     void Start()
     {
         Debug.Log("Le Spawner est sur l'objet : " + gameObject.name);
-        InvokeRepeating("SpawnZone", 10f, 10f);
+        StartCoroutine(CycleZone());
     }
 
-    void SpawnZone()
+    private IEnumerator CycleZone()
     {
-        Debug.Log("ZONE !");
-        float X = Random.Range(-2f, 2f);
-        float Y = Random.Range(-2f, 2f);
-        Vector3 randomPosition = new Vector3(X, Y, 0);
-        GameObject tempZone = Instantiate(quarantinePrefab, randomPosition, Quaternion.identity);
-        tempZone.transform.localScale = new Vector3(tailleZone, tailleZone, 1);
-        Destroy(tempZone, 5f);
+        while (true)
+        {
+            float marge = tailleZone / 2f;
+            float X = Random.Range(MAP_X_MIN + marge, MAP_X_MAX - marge);
+            float Y = Random.Range(MAP_Y_MIN + marge, MAP_Y_MAX - marge);
+            Vector3 randomPosition = new Vector3(X, Y, 0);
+            GameObject tempZone = Instantiate(quarantinePrefab, randomPosition, Quaternion.identity);
+            tempZone.transform.localScale = new Vector3(tailleZone, tailleZone, 1);
+            Debug.Log("ZONE apparue !");
+            yield return new WaitForSeconds(5f);
+
+            Destroy(tempZone);
+            Debug.Log("ZONE disparue !");
+
+            yield return new WaitForSeconds(5f);
+        }
     }
 }
